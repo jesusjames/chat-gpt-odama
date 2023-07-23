c76f815a89def9aca542cbe258408343ae418b42
Jesus Santiago Tvoar <jesus.tovar@embluemail.com>
2023-07-23 00:03:55 -0500

    finished design

--- src/modules/ChatModule/layouts/ChatLayout/ChatLayout.tsx
+++ src/modules/ChatModule/layouts/ChatLayout/ChatLayout.tsx
@@ -1,12 +1,15 @@
 import { memo } from "react"
 import { Outlet } from "react-router-dom"
 import { Header } from "../../components"
+import SideBarProvider from "../../contexts/SideBarContext"
 
 const ChatLayout = () => {
   return (
     <div className="h-screen w-full bg-slate-50">
+      <SideBarProvider>
         <Header />
         <Outlet />
+      </SideBarProvider>
     </div>
   )
 }
--- src/modules/ChatModule/pages/Home/Home.tsx
+++ src/modules/ChatModule/pages/Home/Home.tsx
@@ -1,24 +1,42 @@
+import classNames from 'classnames'
 import { Chat, SearchHistory, SystemCard } from '../../components'
+import { useStateSideBarContext } from '../../contexts/SideBarContext'
 
 type Props = {}
 
 const Home = (props: Props) => {
-  return (
-    <div className='relative h-[calc(100%-93px)] w-full bg-slate-50 p-9'>
-        <div className='flex flex-row h-full w-full gap-5'>
-            <div className='flex flex-col gap-5 basis-1/3'>
-                <SystemCard 
-                    title='Sistema' 
-                    description='Para conseguir una respuesta adecuada a tus necesidades, escribe un prompt para el sistema.' 
-                />
-                <SearchHistory />
-            </div>
-            <div className='basis-2/3'>
-                <Chat />
+    const { hiddenSidebarIsActive } = useStateSideBarContext();
+
+    const classesContainerParent = classNames('relative flex flex-row h-full w-full', {
+        'gap-5': !hiddenSidebarIsActive
+    })
+
+    const classesContainerLeft = classNames('flex flex-col gap-5 transition-all duration-300', {
+        'w-full md:w-1/2 lg:w-1/3 opacity-1 bg-white md:bg-transparent z-10': !hiddenSidebarIsActive,
+        'w-0 overflow-hidden opacity-0': hiddenSidebarIsActive
+    });
+
+    const classesContainerRight = classNames('transition-all duration-300', {
+        'hidden md:w-1/2 lg:w-2/3 md:block': !hiddenSidebarIsActive,
+        'w-full': hiddenSidebarIsActive
+    })
+
+    return (
+        <div className='relative h-[calc(100%-93px)] w-full bg-slate-50 p-9'>
+            <div className={classesContainerParent}>
+                <div className={classesContainerLeft}>
+                    <SystemCard 
+                        title='Sistema' 
+                        description='Para conseguir una respuesta adecuada a tus necesidades, escribe un prompt para el sistema.' 
+                    />
+                    <SearchHistory />
+                </div>
+                <div className={classesContainerRight}>
+                    <Chat />
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Home
